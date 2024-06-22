86a6d37eca38406e1f013f9e8860b16d3484e9da
josephhdu <joseph.kj.du@gmail.com>
2024-06-21 18:54:32 -0700

    react frontend set up, ready for code

---
+++ frontend/src/components/Preferences.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Preferences() {
+  return (
+    <div>
+      <h1>Set Your Preferences</h1>
+      <p>Form to set workout preferences will go here.</p>
+    </div>
+  );
+}
+
+export default Preferences;
---
+++ frontend/src/components/WorkoutRoutine.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WorkoutRoutine() {
+  return (
+    <div>
+      <h1>Your Workout Routine</h1>
+      <p>Generated workout routine will be displayed here.</p>
+    </div>
+  );
+}
+
+export default WorkoutRoutine;
