bf566f6acc6cb7df60d14a72a70914707965546e
ericmah123 <emah2@ualberta.ca>
2024-07-08 23:19:08 -0600

    Front end portion, and merged with previous version

--- fit_planner/workouts/static/workouts/scripts.js
+++ fit_planner/workouts/static/workouts/scripts.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Fit Planner loaded');
+    const splash = document.querySelector('.splash');
+
+    setTimeout(() => {
+        splash.style.opacity = '0';
+    }, 3000); // Display splash screen for 3 seconds
+
+    splash.addEventListener('transitionend', () => {
+        splash.classList.add('display-none');
+        document.getElementById("main-content").classList.remove("hidden");
+    });
 });
+
+
